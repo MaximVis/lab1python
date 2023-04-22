8a0b043de974cd40076be1f7a9f4be4e414b5533
MaximVis <125341611+MaximVis@users.noreply.github.com>
2023-04-22 20:42:08 +0600

    Update Parse.py

--- Parse.py
+++ Parse.py
@@ -6,11 +6,18 @@ def parse():
     url = 'https://omgtu.ru/general_information/faculties/'  # передаем необходимы URL адрес
     page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
     soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
-    block = soup.findAll('div', class_='main__content')  # находим  контейнер с нужным классом
+    blockfacultetov = soup.findAll('div', id='pagecontent')  # находим  контейнер с нужным классом
     description = ''
-    for data in block:  # проходим циклом по содержимому контейнера
-        if data.find('ul'):  # находим тег <ul>
-            description = data.text  # записываем в переменную содержание тега
+    for data in blockfacultetov:#добавление факультетов
+        rows = data.find_all('li')
+    for peremennaya in rows:
+        if peremennaya.find('span'):
+            description += peremennaya.text
+            description += "\n"
+    txtput(description)
+
+
+def txtput(description):
     my_file = open("faculteti.txt", "w")
     my_file.write(description)
     my_file.close()
